45ef632de7dfb95ff9bd20de62ea7c8b4c2ca878
Jeremy Diamond <diamondjbj2@yahoo.com>
2014-03-06 00:44:12 -0800

    Version number

--- src/com/spiteful/forbidden/Forbidden.java
+++ src/com/spiteful/forbidden/Forbidden.java
@@ -29,7 +29,7 @@ import java.util.logging.Level;
 @Mod(
 	modid = "ForbiddenMagic",
 	name = "Forbidden Magic",
-	version = "0.31",
+	version = "0.32",
 	dependencies = "required-after:Thaumcraft;after:Natura;after:ThaumicTinkerer;after:ThaumicExploration"
 )
 @NetworkMod(
